4da60d012ed95857b3bdbad8a8a02e0b1f75f7f0
helder <2190785@my.ipleiria.pt>
2020-11-07 21:46:16 +0000

    PWSAP-97 #done
    Design fragmento obras e a item list

--- RightPrice/app/src/main/java/amsi/dei/estg/ipleiria/RightPrice/Instalador/Conhecer_Fornecedor_Fragment.java
+++ RightPrice/app/src/main/java/amsi/dei/estg/ipleiria/RightPrice/Instalador/Conhecer_Fornecedor_Fragment.java
@@ -15,6 +15,6 @@ public class Conhecer_Fornecedor_Fragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_conhecer__fornecedor_, container, false);
+        return inflater.inflate(R.layout.fragment_conhecer_fornecedor, container, false);
     }
 }
