df7ab257293dec550dd82762067c4426f78046be
Rpla22 <agostinhorui2001@gmail.com>
2020-11-08 15:44:55 +0000

    PWSAP-102 #DONE
    Ciração do framento para adicionar o produto no orçamento

---
+++ RightPrice/app/src/main/java/amsi/dei/estg/ipleiria/RightPrice/Instalador/Adicionar_produto_orcamento.java
@@ -0,0 +1,22 @@
+package amsi.dei.estg.ipleiria.RightPrice.Instalador;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import amsi.dei.estg.ipleiria.RightPrice.R;
+
+public class Adicionar_produto_orcamento extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_adicionar_produto_orcamento, container, false);
+    }
+}
